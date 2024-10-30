d9c3841834ab945430d8f75ee045b8e7e07f340f
Manuel <manuel@strenge.ch>
2024-10-30 19:35:46 +0100

    add lab

--- Lab-ALU_and_Branch_Instructions/bcd/app/main.s
+++ Lab-ALU_and_Branch_Instructions/bcd/app/main.s
@@ -95,7 +95,8 @@ display
 		LDR      R0, =ADDR_7_SEG_BIN_DS3_0
 		STRB     R1, [R0, #1]  
 		STRB     R2, [R0, #0]  
-		B main
+		
+		B disco
 
 display_blue
 		; display blue background
@@ -133,13 +134,66 @@ shift
 		LSLS R3, R3, #3      ; Shift R3 left by 3 bits to get R2 * 8
 		LSLS R4, R2, #1      ; Shift R2 left by 1 to get R2 * 2
 		ADDS R3, R4          ; Add R4 to R3
-		ADDS R3, R1			; add Ones
+		ADDS R3, R1			 ; add Ones
 		BL display_red
 		B combine_bcd
 		
+; disco, counter and increment_counter can be replaced	
+disco
+		LDR 	R0,=ADDR_LED_15_0
+		LDRB 	R0, [R0,#1]		;load binary
+		
+		LDR		R1,=0x0
+		CMP		R0,	R1
+		BEQ		main
+		
+		MOVS    R1, #0           ; R1 will hold the count of ones, start with 0
+		MOVS    R2, #32          ; R2 will act as a loop counter, for 32 bits
+
+count_ones_loop
+		MOVS	R3, #1			; R3 =1
+		TST     R0, R3           ; Test the least significant bit of R0
+		BEQ     skip_increment   ; If the bit is 0, skip the increment
+		ADDS   R1, R1, #1       ; If the least significant bit is 1, increment the count in R1
+skip_increment
+		LSRS     R0, R0, #1       ; Logical shift right R0 by 1 to check the next bit
+		SUBS    R2, R2, #1       ; Decrement the loop counter in R2
+		BNE     count_ones_loop  ; Repeat until all 32 bits are checked
+
+; R1 contains the count
+		MOVS     R0, #0           ; Clear R0 to start with zeroes
+		MOVS     R2, R1           ; R2 will act as a counter for setting the initial bits
+
+set_ones_loop
+		MOVS	R3, #1
+		ORRS     R0, R0, R3       ; Set the least significant bit in R0
+		LSLS     R0, R0, #1       ; Shift R0 left to prepare for the next bit
+		SUBS    R2, R2, #1       ; Decrement the counter R2
+		BNE     set_ones_loop    ; Repeat until we set 'count' bits in R0
+		MOV     R4, R0           ; Save the original pattern in R3 for comparison
+		LSLS 	R0, R0, #16		 ;shift it back a halfword
+		ORRS 	R0, R0, R4		 ; combine shifted plus old one
+		MOV     R4, R0           ; Save the new original pattern in R3 for comparison
+		
+		
+
+
+    ; At this point, R0 contains the specified number of ones in the most significant bits
+    ; Now enter the rotation loop to continuously rotate R0 and call the pause function
+	; now duplicate it do 
+	
+
+rotate_loop
+		RORS    R0, R0, R3       ; Rotate R0 left by 1 with overspill from the MSB to LSB
+		LDR		R1,=ADDR_LED_31_16
+		STRH	R0,	[R1]
+		BL   	pause            ; Call the pause subroutine
+		CMP		R0, R4           ; Compare the current pattern in R0 with the original in R3
+		BNE		rotate_loop      ; Repeat indefinitely or as per your loop condition
+		B main
+		
 
 ; END: To be programmed
-
         B       main
         ENDP
             
